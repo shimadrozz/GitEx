6b83b5f7cec06e6921dbb573bd836c8d9ddc7203
Shima Drozz <shimadrozz@gmail.com>
2024-08-28 03:51:30 -0400

    American guy commit

--- src/files/ReUsableMethods.java
+++ src/files/ReUsableMethods.java
@@ -7,6 +7,7 @@ public class ReUsableMethods {
 	public static JsonPath rawToJson(String response)
 	{
 		JsonPath js1 = new JsonPath(response);
+		System.out.println("Web Automation");
 		return js1;
 	}
 
